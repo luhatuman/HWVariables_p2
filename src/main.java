e1dc1390dbd1389fc2a02eff2807f0acfe660c58
itsOtto <luhatuman@yandex.ru>
2022-11-13 22:31:49 +0900

    Task 1

--- src/main.java
+++ src/main.java
@@ -1,5 +1,13 @@
 public class main {
     public static void main(String[] args) {
-                System.out.println();
+        //Task 1
+        System.out.println("Task 1");
+        byte aA = 127;
+        short bB = 32767;
+        int cC = 2147483647;
+        long dD = 9223372036854775807L;
+        float eE = 2.5f;
+        double fF = 3.4465465434654;
+        System.out.println(aA + "\n" + bB + "\n" + cC + "\n" + dD + "\n" + eE + "\n" + fF);
     }
 }
