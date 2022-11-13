172b4e0ce6242922b45334f9ad6b944056eb74e1
itsOtto <luhatuman@yandex.ru>
2022-11-13 22:49:28 +0900

    Task 2

--- src/main.java
+++ src/main.java
@@ -9,5 +9,18 @@ public class main {
         float eE = 2.5f;
         double fF = 3.4465465434654;
         System.out.println(aA + "\n" + bB + "\n" + cC + "\n" + dD + "\n" + eE + "\n" + fF);
+
+        //Task 2
+        System.out.println("Task 2");
+        byte aA1 = 2;
+        byte aA2 = 67;
+        short bB1 = -159;
+        short bB2 = 569;
+        short bB3 = 786;
+        short bB4 = 27897;
+        long cC1 = 987678965549L;
+        float dD1 = 27.12f;
+        boolean eE1 = false;
+        System.out.println(aA1 + "\n" + aA2 + "\n" + bB1 + "\n" + bB2 + "\n" + bB3 + "\n" + bB4 + "\n" + cC1 + "\n" + dD1 + "\n" + eE1);
             }
 }
