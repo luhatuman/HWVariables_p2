68c6dfb19cbbb66bd42aa6cfdf799cb464af5fb0
itsOtto <luhatuman@yandex.ru>
2022-11-13 22:35:12 +0900

    Task 1

--- src/main.java
+++ src/main.java
@@ -9,5 +9,5 @@ public class main {
         float eE = 2.5f;
         double fF = 3.4465465434654;
         System.out.println(aA + "\n" + bB + "\n" + cC + "\n" + dD + "\n" + eE + "\n" + fF);
-    }
+            }
 }
